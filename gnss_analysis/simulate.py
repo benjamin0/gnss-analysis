beacba56bd7ef37af41fb6b0bd14db2618152622
Alex Kleeman <akleeman@gmail.com>
2015-11-23 11:40:04 -0800

    Changed the structure a bit, the 'state' is now a dictionary which  keeps track of the rover, base and ephemeris observations.  The  rover and base observations are overwritten with each new observation,  the ephemeris are accumulated.

--- gnss_analysis/simulate.py
+++ gnss_analysis/simulate.py
@@ -15,6 +15,7 @@ files and simulate the chip state which can be used to perform
 analysis and iterate on the algorithm.
 """
 
+import copy
 import time
 import logging
 import datetime
@@ -23,20 +24,10 @@ import pandas as pd
 
 import sbp.observation as ob
 
+from swiftnav import gpstime
 from sbp.utils import exclude_fields
-from swiftnav.gpstime import gpst_components2datetime
 
 import gnss_analysis.constants as c
-
-
-def time_from_message(wn, tow):
-  """
-  Takes a message with a time field stored as week number and time
-  of week and converts it to a TimeStamp.
-  """
-  # compute the time from the week number and time of week
-  dtime = gpst_components2datetime(wn, tow / c.MSEC_TO_SECONDS)
-  return dtime
 
 
 def get_source(msg):
@@ -84,22 +75,12 @@ def observation_to_dataframe(msg, data):
   -------
   df : pd.DataFrame
     A DataFrame holding all the observations from various
-    satellites.  The DataFrame is indexed by source and
-    satellite, ('rover', 16) for example.  The DataFrame
-    holds pseudorange, carrier phase
+    satellites.  The DataFrame is indexed by satellite id
+    and holds information such as the gps time and pseudorange
+    carrier phase observations.
   """
   # make sure the message is an observation message
   assert isinstance(msg, (ob.MsgObs, ob.MsgObsDepA))
-  # determine the source
-  source = get_source(msg)
-
-  # determine the time from week number and time of week
-  dtime = time_from_message(msg.header.t.wn, msg.header.t.tow)
-  # converts to seconds from epoch.  This keeps all the data
-  # in the resulting DataFrame floats which makes the
-  # subsequent update steps a lot faster (since we can avoid
-  # the overhead of datetime64 logic).
-  timestamp = time.mktime(dtime.timetuple())
 
   def extract_observations(obs):
       # Convert pseudorange, carrier phase to SI units.
@@ -109,18 +90,16 @@ def observation_to_dataframe(msg, data):
            'lock': obs.lock,
            'host_time': data['timestamp'],
            'host_offset': data['delta'],
-           'timestamp': timestamp,
+           'wn': msg.header.t.wn,
+           # notice that in observations the tow is in msecs
+           'tow': msg.header.t.tow / c.MSEC_TO_SECONDS,
            }
       return v
 
-  # Compute the indices of each of the observations
-  source_sid_pairs = [(source, get_sid(o)) for o in msg.obs]
-  # assemble into a MultiIndex
-  idx = pd.MultiIndex.from_tuples(source_sid_pairs,
-                                  names=['source', 'sid'])
   # Combine into a dataframe.
   df = pd.DataFrame([extract_observations(o) for o in msg.obs],
-                    index=idx)
+                    index=pd.Index([get_sid(o) for o in msg.obs],
+                                   name='sid'))
   return df
 
 
@@ -139,25 +118,23 @@ def ephemeris_to_dataframe(msg, data):
   Returns
   -------
   eph : pd.DataFrame
-    A DataFrame indexed by the source and satellite id, ('rover', 16)
-    for example.  The columns then hold the ephemeris parameters.
+    A DataFrame indexed by the satellite id and holds the
+    ephemeris parameters for each satellite.
   """
   assert isinstance(msg, (ob.MsgEphemeris,
                           ob.MsgEphemerisDepA,
                           ob.MsgEphemerisDepB))
-  # determine if the ephemeris was from the rover or base
-  # determine the satellite id.
   # if the message is healthy and valid we emit the corresponding DataFrame
   if msg.healthy == 1 and msg.valid == 1:
+    # determine if the ephemeris was from the rover or base
     source = get_source(msg)
+    # determine the satellite id.
     sid = get_sid(msg)
     msg = exclude_fields(msg)
-    # make sure the satellite id attribute isn't propagated
+    # make sure the satellite id attribute isn't propagated since
+    # it is part of the index
     [msg.pop(x, None) for x in ['sid', 'prn']]
-    # create the MultiIndex and DataFrame
-    idx = pd.MultiIndex.from_tuples([(source, sid)],
-                                    names=['source', 'sid'])
-    eph = pd.DataFrame(msg, index=idx)
+    eph = pd.DataFrame(msg, index=pd.Index([sid], name='sid'))
     return eph
 
 
@@ -170,10 +147,10 @@ def tdcp_doppler(old, new):
   -----------
   old : pd.DataFrame
     A DataFrame holding a previous set of carrier phase
-    observations along with corresponding timestamps
+    observations along with corresponding tow
   new : pd.DataFrame
     A DataFrame holding new carrier phase observations
-    along with corresponding time stamps
+    along with corresponding tow
 
   Returns:
   --------
@@ -182,121 +159,124 @@ def tdcp_doppler(old, new):
 
   See also: libswiftnav/src/track.c:tdcp_doppler
   """
+  # TODO: make sure the week numbers are the same since
+  # (at least for now) we don't take them into account
+
   # delta time in seconds
-  dt = new['timestamp'] - old['timestamp']
-  # make sure dt is non-negative.  Sometimes this
-  # is zero ?!  In which case should we set the doppler
-  # to NaN?  Or use a previously available doppler?
-  assert np.all(dt.values >= 0.)
+  dt = new['tow'] - old['tow']
+  # make sure dt is positive.
+  assert not np.any(dt.values <= 0.)
   # compute the rate of change of the carrier phase
   doppler = (new.L - old.L) / dt
+  # mark any computations with differing locks as nan
+  invalid = new['lock'] != old['lock']
+  doppler[invalid] = np.nan
   return doppler
 
 
-def add_doppler(state, updates):
+def update_ephemeris(state, msg, data):
   """
-  Computes doppler information from the difference between
-  a current state estimate and any updates.  The result
-  is stored as a new column 'doppler' in the update DataFrame.
-
-  Parameters
-  ----------
-  state : pd.DataFrame
-    A DataFrame which holds the current estimate of state before
-    any updates have been applied.
-  updates : pd.DataFrame
-    A DataFrame which holds any updates resulting from a new
-    log message.
-
-  Returns
-  -------
-  updates : pd.DataFrame
-    A data frame with an additional column holding the doppler
-    information if lock information existed.  Otherwise it
-    returns an unmodified updates DataFrame.
+  Updates the current state with a new ephemeris message.
+  An ephemeris message typically contains the
+  ephemeris for a single satellite.  This new ephemeris
+  information is added to the previous ephemerides,
+  overwriting if necessary.
   """
-  # make sure the two DataFrames have already been aligned.
-  assert state.index.equals(updates.index)
-  # if both the current state and the update contain
-  # lock counts, we can compute the doppler shift and
-  # do so only for cases where the lock counts haven't changed.
-  if 'lock' in state and 'lock' in updates:
-    use = state['lock'] == updates['lock']
-    if np.any(use):
-      # if we lose a lock, should we set doppler to nan, or
-      # keep the old value?
-      updates['doppler'] = tdcp_doppler(state.ix[use],
-                                        updates.ix[use])
-  return updates
-
-
-def initial_state():
-  return None
-
-
-def update_state(state, updates):
-  """
-  Takes a previous state and an update to the state and
-  combines the two.  This also computes any derived quantities
-  that require differencing the two states (such as doppler).
-  """
+  updates = ephemeris_to_dataframe(msg, data)
+  prev_ephs = state['ephemeris']
   # if no updates, return the original state
   if updates is None:
     return state
   # if there is no previous state return the updates.
-  if state is None:
-    return updates
+  if prev_ephs.empty:
+    state['ephemeris'] = updates
+    return state
   # doing align upfront avoid's doing alignment twice
-  updates, state = updates.align(state)
-  # infer doppler information and add it to the updates
-  updates = add_doppler(state, updates)
-  # update the state (or initialize it)
-
+  updates, prev_ephs = updates.align(prev_ephs)
   # pd.DataFrame.combine_first(updates, state) would
   # work here as well, but it is extremely slow.  This
   # version is not nearly as robust but is an order
   # of magnitude faster.
-  updates[updates.isnull()] = state
-  return updates
+  updates[updates.isnull()] = prev_ephs
+  state['ephemeris'] = updates
+  return state
 
 
-def simulate_from_log(log):
+def update_observation(state, msg, data):
   """
-  Iterates through a log file and emits a pandas DataFrame
-  that holds the current observation state.
+  Updates the current state with a new set of observations.
+  An observation message typically contains information
+  from multiple satellites.  When new observations are
+  received the previous observations are simply thrown out,
+  so this method replaces the old obs with the new obs.
+  """
+  new_obs = observation_to_dataframe(msg, data)
+  # determine if the message was from the rover or base and
+  # get the previous observations.
+  source = get_source(msg)
+  prev_obs = state[source]
+  # align the previous observations to match the new_obs index.
+  obs, prev_obs = new_obs.align(prev_obs, 'left')
+  # update the doppler information.
+  new_obs['doppler'] = tdcp_doppler(prev_obs, new_obs)
+  return state
+
+
+def is_rover_observation(msg):
+  """
+  Returns true if msg is an observation type and it came
+  from the rover.
+  """
+  return isinstance(msg, (ob.MsgObs, ob.MsgObsDepA)) and msg.sender
+
+
+def simulate_from_log(log, initial_state=None):
+  """
+  Iterates through a log file and emits a dictionary
+  of DataFrames holding the current state of observations
+  for each rover observation.
 
   Parameters
   ----------
   log : sbp.base_logger.BaseLogger
     An sbp log iterator that should yield msg, data pairs.
+  initial_state : (optional) dict
+    An optional dictionary containing the state at the
+    beginning of logging.
 
   Returns
   -------
   states : generator
-    A generator which yields pandas DataFrames with the
-    current estimate of state.  Each DataFrame is indexed
-    by source and sid, ('rover', 16) for example, and contains
-    the most recent observations and ephemeris information for
-    each of the satellites.
-  """
-  _processors = {ob.MsgObs: observation_to_dataframe,
-                 ob.MsgObsDepA: observation_to_dataframe,
-                 ob.MsgEphemeris: ephemeris_to_dataframe,
-                 ob.MsgEphemerisDepA: ephemeris_to_dataframe,
-                 ob.MsgEphemerisDepB: ephemeris_to_dataframe}
+    A generator which yields current estimates of state.
+    Each state is keyed by source ('rover', 'base', 'ephemeris')
+    and then indexed by satellite id.  For example,
+    state['rover'][16] would contain a DataFrame holding
+    the rover stations observations of satellite 16.
 
-  state = initial_state()
+    A state is only emitted for every rover observation.
+  """
+  _processors = {ob.MsgObs: update_observation,
+                 ob.MsgObsDepA: update_observation,
+                 ob.MsgEphemeris: update_ephemeris,
+                 ob.MsgEphemerisDepA: update_ephemeris,
+                 ob.MsgEphemerisDepB: update_ephemeris}
+
+  # use either the initial state provided, or a blank set
+  # of observations.
+  state = initial_state or {'rover': pd.DataFrame(),
+                            'base': pd.DataFrame(),
+                            'ephemeris': pd.DataFrame()}
+
   # what's up with iterating over log.next()?
   for msg, data in log.next():
     if type(msg) in _processors:
-      updates = _processors[type(msg)](msg, data)
-      # TODO: take care of integrity checks
       # TODO: process other messages
+      state = _processors[type(msg)](state, msg, data)
+      # TODO: take care of integrity checks
       # TODO: remove out-dated information?
-      state = update_state(state, updates)
-      if state is not None:
+      if is_rover_observation(msg) and state is not None:
         # yield a copy so we don't accidentally modify things.
-        yield state.copy()
+        yield copy.deepcopy(state)
     else:
       logging.debug("No processor available for message type %s"
                     % type(msg))
--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,8 +1,12 @@
 
+import copy
+import time
 import pytest
+import datetime
 import numpy as np
 
 from gnss_analysis import simulate
+from swiftnav import gpstime
 
 
 def test_simulate_from_log(jsonlog):
@@ -15,12 +19,13 @@ def test_simulate_from_log(jsonlog):
   states = simulate.simulate_from_log(jsonlog)
   prev_state = states.next()
   for state in states:
-    prev_state_copy = prev_state.copy()
-    state_copy = state.copy()
-    # fill with nans
-    state.ix[:] = np.nan
-    # make sure modifying the current state won't
-    # alter the previous state
-    np.testing.assert_array_equal(prev_state.values,
-                                  prev_state_copy.values)
+    state_copy = copy.deepcopy(state)
+    for k, new_v in state.iteritems():
+      prev_v_copy = copy.deepcopy(prev_state[k])
+      # fill with nans
+      new_v.ix[:] = np.nan
+      # make sure modifying the current state won't
+      # alter the previous state
+      np.testing.assert_array_equal(prev_state[k].values,
+                                    prev_v_copy.values)
     prev_state = state_copy
